c083e18bd147180d63769e892b4140e061abfe57
pikcsay <pikcsay@gmail.com>
2020-11-21 17:07:26 +0300

    )

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 from random import randrange
 from PyQt5.QtGui import QBrush, QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import uic
+from ui import Ui_Form
 
 SCREEN_SIZE = [500, 500]
 
 
-class Example(QWidget):
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui.ui', self)
+        self.setupUi(self)
         self.setGeometry(300, 300, *SCREEN_SIZE)
         self.do_paint = False
         self.button.clicked.connect(self.draw_on)
@@ -27,7 +27,7 @@ class Example(QWidget):
         self.repaint()
 
     def draw(self, qp):
-        brush = QBrush(QColor('yellow'))
+        brush = QBrush(QColor(f'#%6x' % randrange(0, 0xFFFFFF)))
         r = randrange(1, SCREEN_SIZE[0] * 0.5)
         x, y = 250, 200
         qp.setBrush(brush)
